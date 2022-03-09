a06c685e4d3dfecea112261b896169a801f237e2
madhurtoppo <madhur.toppo@gmail.com>
2022-03-09 22:11:01 +0530

    Added tests for multiplication service

--- multiplication/src/test/java/com/madhurtoppo/multiplication/challenge/controllers/ChallengeAttemptControllerTest.java
+++ multiplication/src/test/java/com/madhurtoppo/multiplication/challenge/controllers/ChallengeAttemptControllerTest.java
@@ -1,6 +1,5 @@
 package com.madhurtoppo.multiplication.challenge.controllers;
 
-//import com.madhurtoppo.multiplication.challenge.dto.ChallengeAttemptDTO;
 import com.madhurtoppo.multiplication.challenge.domain.Challenge;
 import com.madhurtoppo.multiplication.challenge.domain.ChallengeAttempt;
 import com.madhurtoppo.multiplication.challenge.domain.ChallengeAttemptDTO;
@@ -19,9 +18,12 @@ import org.springframework.mock.web.MockHttpServletResponse;
 import org.springframework.test.context.junit.jupiter.SpringExtension;
 import org.springframework.test.web.servlet.MockMvc;
 
+import java.util.List;
+
 import static org.assertj.core.api.BDDAssertions.then;
 import static org.mockito.ArgumentMatchers.eq;
 import static org.mockito.BDDMockito.given;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
 
 @ExtendWith(SpringExtension.class)
@@ -34,6 +36,8 @@ class ChallengeAttemptControllerTest {
     @Autowired private MockMvc mvc;
     @Autowired private JacksonTester<ChallengeAttemptDTO> jsonRequestAttempt;
     @Autowired private JacksonTester<ChallengeAttempt> jsonResultAttempt;
+    @Autowired private JacksonTester<List<ChallengeAttempt>> jsonResultAttemptList;
+
 
     @Test
     void postValidResult() throws Exception {
@@ -41,7 +45,6 @@ class ChallengeAttemptControllerTest {
         User user = new User(1L, "john");
         Challenge challenge = new Challenge(50, 70);
         ChallengeAttemptDTO attemptDTO = new ChallengeAttemptDTO(50, 70, "john", 3500);
-//        ChallengeAttempt expectedResponse = new ChallengeAttempt(5L, user, 50, 70, 3500, true);
         ChallengeAttempt expectedResponse = new ChallengeAttempt(5L, user, challenge, 3500, true);
         given(challengeService.verifyAttempt(eq(attemptDTO))).willReturn(expectedResponse);
 
@@ -69,5 +72,26 @@ class ChallengeAttemptControllerTest {
 
         // then
         then(response.getStatus()).isEqualTo(HttpStatus.BAD_REQUEST.value());
+    }
+
+    @Test
+    public void getUserStats() throws Exception {
+        // given
+        User user = new User("john_doe");
+        Challenge challenge1 = new Challenge(50, 70);
+        Challenge challenge2 = new Challenge(20, 10);
+        ChallengeAttempt attempt1 = new ChallengeAttempt(1L, user, challenge1, 3500, true);
+        ChallengeAttempt attempt2 = new ChallengeAttempt(2L, user, challenge2, 210, false);
+        List<ChallengeAttempt> recentAttempts = List.of(attempt1, attempt2);
+        given(challengeService.getStatsForUser("john_doe")).willReturn(recentAttempts);
+
+        // when
+        MockHttpServletResponse response = mvc.perform(get("/attempts/{userAlias}", user.getAlias()))
+                .andReturn()
+                .getResponse();
+
+        // then
+        then(response.getStatus()).isEqualTo(HttpStatus.OK.value());
+        then(response.getContentAsString()).isEqualTo(jsonResultAttemptList.write(recentAttempts).getJson());
     }
 }
--- multiplication/src/test/java/com/madhurtoppo/multiplication/challenge/services/ChallengeServiceTest.java
+++ multiplication/src/test/java/com/madhurtoppo/multiplication/challenge/services/ChallengeServiceTest.java
@@ -1,7 +1,7 @@
 package com.madhurtoppo.multiplication.challenge.services;
 
+import com.madhurtoppo.multiplication.challenge.domain.Challenge;
 import com.madhurtoppo.multiplication.challenge.domain.ChallengeAttempt;
-//import com.madhurtoppo.multiplication.challenge.dto.ChallengeAttemptDTO;
 import com.madhurtoppo.multiplication.challenge.domain.ChallengeAttemptDTO;
 import com.madhurtoppo.multiplication.challenge.repositories.ChallengeAttemptRepository;
 import com.madhurtoppo.multiplication.challenge.repositories.ChallengeRepository;
@@ -14,6 +14,7 @@ import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.Mock;
 import org.mockito.junit.jupiter.MockitoExtension;
 
+import java.util.List;
 import java.util.Optional;
 
 import static org.assertj.core.api.BDDAssertions.then;
@@ -29,11 +30,8 @@ class ChallengeServiceTest {
     private ChallengeService challengeService;
 
     @Mock private UserRepository userRepository;
-
     @Mock private ChallengeAttemptRepository attemptRepository;
-
     @Mock private ChallengeRepository challengeRepository;
-
     @Mock private GamificationServiceClient gamificationServiceClient;
 
     @BeforeEach
@@ -42,12 +40,12 @@ class ChallengeServiceTest {
                                                     challengeRepository,
                                                     attemptRepository,
                                                     gamificationServiceClient);
-        given(attemptRepository.save(any())).will(returnsFirstArg());
     }
 
     @Test
     void checkCorrectAttemptTest() {
         // given
+        given(attemptRepository.save(any())).will(returnsFirstArg());
         ChallengeAttemptDTO attemptDTO = new ChallengeAttemptDTO(50, 60, "Madhur Toppo", 3000);
 
         // when
@@ -55,7 +53,6 @@ class ChallengeServiceTest {
 
         // then
         then(resultAttempt.isCorrect()).isTrue();
-
         verify(userRepository).save(new User("Madhur Toppo"));
         verify(attemptRepository).save(resultAttempt);
         verify(gamificationServiceClient).sendAttempt(resultAttempt);
@@ -64,6 +61,7 @@ class ChallengeServiceTest {
     @Test
     void checkWrongAttemptTest() {
         // given
+        given(attemptRepository.save(any())).will(returnsFirstArg());
         ChallengeAttemptDTO attemptDTO = new ChallengeAttemptDTO(50, 60, "Mukul Toppo", 4000);
 
         // when
@@ -76,6 +74,7 @@ class ChallengeServiceTest {
     @Test
     void checkExistingUserTest() {
         // given
+        given(attemptRepository.save(any())).will(returnsFirstArg());
         User existingUser = new User(1L, "john_doe");
         given(userRepository.findByAlias("john_doe")).willReturn(Optional.of(existingUser));
         ChallengeAttemptDTO attemptDTO = new ChallengeAttemptDTO(50, 60, "john_doe", 5000);
@@ -88,5 +87,23 @@ class ChallengeServiceTest {
         then(resultAttempt.getUser()).isEqualTo(existingUser);
         verify(userRepository, never()).save(any());
         verify(attemptRepository).save(resultAttempt);
+    }
+
+    @Test
+    public void retrieveStatsTest() {
+        // given
+        User user = new User("john_doe");
+        Challenge challenge1 = new Challenge(50, 60);
+        Challenge challenge2 = new Challenge(50, 60);
+        ChallengeAttempt attempt1 = new ChallengeAttempt(1L, user, challenge1, 3010, false);
+        ChallengeAttempt attempt2 = new ChallengeAttempt(2L, user, challenge2, 3051, false);
+        List<ChallengeAttempt> lastAttempts = List.of(attempt1, attempt2);
+        given(attemptRepository.findTop10ByUserAliasOrderByIdDesc("john_doe")).willReturn(lastAttempts);
+
+        // when
+        List<ChallengeAttempt> latestAttemptsResult = challengeService.getStatsForUser("john_doe");
+
+        // then
+        then(latestAttemptsResult).isEqualTo(lastAttempts);
     }
 }
