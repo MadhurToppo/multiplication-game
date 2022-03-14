8098c8d1e0fdd6ff3417f04a67f0d5fcc085032b
madhurtoppo <madhur.toppo@gmail.com>
2022-03-14 18:49:58 +0530

    Added Gateway for both services

--- multiplication/challenges-frontend/src/App.js
+++
@@ -1,9 +0,0 @@
-import React from "react";
-import './App.css';
-import ChallengeComponent from "./components/ChallengeComponent";
-
-function App() {
-  return <ChallengeComponent/>;
-}
-
-export default App;
